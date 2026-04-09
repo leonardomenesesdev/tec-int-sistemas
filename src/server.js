600755feed63adf26999813b4179e6d235abf94c
leo nitro <leonardomenesesc@edu.unifor.br>
2026-04-09 06:11:44 -0300

    fix: server.js

--- src/server.js
+++ src/server.js
@@ -3,12 +3,13 @@ import express from 'express';
 import cors from 'cors';
 import userRoutes from './routes/userRoutes.js';
 import veiculoRoutes from './routes/veiculoRoutes.js';
+import alocacaoRoutes from './routes/alocacaoRoutes.js';
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/users', userRoutes);
 app.use('/api/veiculos', veiculoRoutes);
-
+app.use('/api/alocacoes', alocacaoRoutes);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
